4e42ece6bf8f1f88132b70c849b36fae604d541f
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2023-09-23 13:07:11 +0530

    Add expenditure-analysis solution
    Added solution to the expenditure-analysis.js problem

--- week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/expenditure-analysis.js
@@ -1,6 +1,6 @@
 /*
   Implement a function `calculateTotalSpentByCategory` which takes a list of transactions as parameter
-  and return a list of objects where each object is unique category-wise and has total price spent as its value.
+  and returns a list of objects where each object is unique category-wise and has total price spent as its value.
   Transaction - an object like { itemName, category, price, timestamp }.
   Output - [{ category1 - total_amount_spent_on_category1 }, { category2 - total_amount_spent_on_category2 }]
 
@@ -8,8 +8,36 @@
   - `npm run test-expenditure-analysis`
 */
 
-function calculateTotalSpentByCategory(transactions) {
-  return [];
+function calculateTotalSpentByCategory (transactions)
+{
+  const transactionHistoryMap = new Map();
+  for (const transaction of transactions)
+  {
+    const currentCategory = transaction.category
+    const currentAmountSpent = transaction.price
+
+    if (transactionHistoryMap. has(currentCategory))
+    {
+      transactionHistoryMap.set (currentCategory, transactionHistoryMap.get (currentCategory) + currentAmountSpent);
+    }
+    else
+    {
+      transactionHistoryMap.set (currentCategory, currentAmountSpent);
+    }
+  }
+
+  const transactionHistory = [];
+  const transactionHistorySize = transactionHistoryMap.length;
+  for (const [categoryName, totalAmountSpent] of transactionHistoryMap)
+  {
+    const currentTransaction = {
+      'category': categoryName,
+      'totalSpent': totalAmountSpent,
+    };
+
+    transactionHistory.push (currentTransaction);
+  }
+  return transactionHistory;
 }
 
 module.exports = calculateTotalSpentByCategory;
