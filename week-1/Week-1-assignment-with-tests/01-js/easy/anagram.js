102cc3ced1b9c2d9fac63f91a42e11edff86fbc0
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2023-09-23 11:46:59 +0530

    add anagram solution
    Added solution to the anagram.js problem

--- week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
+++ week-1/Week-1-assignment-with-tests/01-js/easy/anagram.js
@@ -7,8 +7,30 @@
   - `npm run test-anagram`
 */
 
-function isAnagram(str1, str2) {
+function isAnagram (str1, str2)
+{
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  
+  const s1 = str1.split("");
+  const s2 = str2.split("");
 
+  s1.sort();
+  s2.sort();
+
+  if (s1.length != s2.length)
+  {
+    return false;
+  }
+
+  for (var i = 0; i < s1.length; ++i)
+  {
+    if ((s1[i]) != s2[i])
+    {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isAnagram;
