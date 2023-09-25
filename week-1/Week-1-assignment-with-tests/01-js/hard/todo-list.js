8f8074074ef98787e2c0a5f1dcfa0c1053f0c5ee
Mohd Abbas Haidar <the-hdr@users.noreply.github.com>
2023-09-25 18:00:01 +0530

    add solution
    Added solution to todo-list.js problem

--- week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
+++ week-1/Week-1-assignment-with-tests/01-js/hard/todo-list.js
@@ -11,8 +11,66 @@
   - `npm run test-todo-list`
 */
 
-class Todo {
+class Todo
+{
+  constructor()
+  {
+    this.todoList = [];
+  }
 
+  add (task)
+  {
+    this.todoList.push (task);
+  }
+
+  remove (indexOfTask)
+  {
+    const todoListLength = this.todoList.length;
+
+    if (indexOfTask >= todoListLength)
+    {
+      return null;
+    }
+
+    for (let i = indexOfTask; i <= todoListLength - 2; ++i)
+    {
+      this.todoList[i] = this.todoList[i + 1];
+    }
+    this.todoList.pop();
+    return null;
+  }
+
+  update (updateIndex, updatedTodo)
+  {
+    const todoListSize = this.todoList.length;
+    if (updateIndex >= todoListSize)
+    {
+      return null;
+    }
+    return this.todoList[updateIndex] = updatedTodo;
+  }
+
+  getAll ()
+  {
+    return this.todoList;
+  }
+
+  get (demandedIndex)
+  {
+    if (demandedIndex >= this.todoList.length)
+    {
+      return null;
+    }
+
+    return this.todoList[demandedIndex];
+  }
+
+  clear ()
+  {
+    this.todoList = [];
+  }
 }
+
+let todo = new Todo();
 
 module.exports = Todo;
